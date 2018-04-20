4133c554c7c1eabf23bf575252c5d954d1bbe93e
ashleygrudzinski <apg3@illinois.edu>
2018-04-20 14:59:04 -0500

    I hope this works.

--- app/src/main/java/edu/illinois/cs/cs125/wow/MainActivity.java
+++ app/src/main/java/edu/illinois/cs/cs125/wow/MainActivity.java
@@ -9,12 +9,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        /**
-         * edit.
-         */
 
-        // yay
 
-        //one more time
+
     }
 }
