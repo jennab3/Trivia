23da402b981a98ea54b5cccbd2631fe2a41f5c39
ashleygrudzinski <apg3@illinois.edu>
2018-04-22 15:09:59 -0500

    Added the buttons for the trivia questions, as well as the onClick things

--- app/src/main/java/edu/illinois/cs/cs125/wow/MainActivity.java
+++ app/src/main/java/edu/illinois/cs/cs125/wow/MainActivity.java
@@ -2,15 +2,98 @@ package edu.illinois.cs.cs125.wow;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Button;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "WOW:Main";
+
+    public RequestQueue requestQ;
+
+    public Button answerOne, answerTwo, answerThree, answerFour;
+
+    public TextView question;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        answerOne = (Button) findViewById(R.id.option1);
+        answerTwo = (Button) findViewById(R.id.option2);
+        answerThree = (Button) findViewById(R.id.option3);
+        answerFour = (Button) findViewById(R.id.option4);
+        question = (TextView) findViewById(R.id.question);
 
+        answerOne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+            }
+        });
+
+        answerTwo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        answerThree.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        answerFour.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        startAPICall();
     }
+
+    void startAPICall() {
+        try {
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                    Request.Method.GET,
+                    "https://opentdb.com/api.php?amount=10&type=multiple",
+                    null,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(final JSONObject response) {
+                            Log.d(TAG, response.toString());
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(final VolleyError error) {
+                    Log.w(TAG, error.toString());
+                }
+            });
+            requestQ.add(jsonObjectRequest);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
